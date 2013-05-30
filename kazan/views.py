0a06b89b287dbf43d737a50c428b45c6267f9f15
Ivan Ivashchenko <ivan.ivashchenko@7bits.it>
2013-05-30 19:04:34 +0700

    second commit

--- kazan/views.py
+++ kazan/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 
-from kazan.models import Ad
+from kazan.models import Ad, User
+
 
 def index(request):
 
@@ -10,7 +11,15 @@ def index(request):
     return render(request, 'kazan/index.html', context)
 
 def user_detail(request, user_id):
-    return HttpResponse("You're looking at user %s." % user_id)
+
+    user = get_object_or_404(User, id=user_id)
+    return render(request, 'kazan/user_detail.html', {'user': user})
 
 def ad_detail(request, ad_id):
-    return HttpResponse("You're looking at ad %s." % ad_id)
+
+    ad = get_object_or_404(Ad, id=ad_id)
+    return render(request, 'kazan/ad_detail.html', {'ad': ad})
+
+def login(request):
+
+    
